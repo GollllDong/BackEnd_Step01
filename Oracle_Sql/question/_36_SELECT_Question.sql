b4b4ffe84af8abee4ace4cae6655a8da11b2613e
GollllDong <wjdqjatnwkd@naver.com>
2024-04-16 15:32:02 +0900

    DML 문제 풀이 및 테이블 생성

--- Oracle_Sql/question/_36_SELECT_Question.sql
+++ Oracle_Sql/question/_36_SELECT_Question.sql
@@ -1,10 +1,24 @@
-
 -- 1) 화학과 학생과 평점이 동일한 학생들을 검색하세요
-SELECT DISTINCT s1.sname, s1.AVR, s2.sname, s2.avr
- from student s1, student s2
- WHERE s1.major = '화학' AND s2.major ='화학' 
- AND s1.avr = s2.avr;
--- 2) 화학과 교수와 부임일이 같은 직원을 검색하세요
+SELECT sno, sname, major, avr
+FROM student
+WHERE avr IN (SELECT avr
+			  FROM student
+			  WHERE major='화학')
+AND major!='화학'
+ORDER BY major;
 
+-- 2) 화학과 교수와 부임일이 같은 직원을 검색하세요
+SELECT eno, ename, hiredate
+FROM emp
+WHERE hiredate IN (SELECT hiredate
+				   FROM professor
+				   WHERE section='화학');
 
 -- 3) 화학과 학생과 같은 학년에서 평점이 동일한 학생들을 검색하세요
+SELECT sno, sname, major, syear, avr
+FROM student
+WHERE (syear, avr) IN (SELECT syear, avr
+					   FROM student
+					   WHERE major='화학')
+AND major!='화학'
+ORDER BY major;
